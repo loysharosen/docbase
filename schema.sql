679cfe70c53c09444de14b52220ba28a574d31b2
Felix Loysha-Rosén <rosenfelix@gmail.com>
2025-06-15 23:11:51 +0200

    major revision

--- schema.sql
+++ schema.sql
@@ -1,25 +1,25 @@
-CREATE TABLE docs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE,
-    abbreviation TEXT,
-    description TEXT NOT NULL,
-    keywords TEXT,
-    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS docs (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(80) NOT NULL UNIQUE,
+    abbreviation VARCHAR(30),
+    description VARCHAR(255) NOT NULL,
+    keywords VARCHAR(255),
+    date_added TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Europe/Stockholm')
 );
 
-CREATE TABLE links (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS links (
+    id SERIAL PRIMARY KEY,
     doc_id INTEGER NOT NULL,
     docbase_link TEXT,
     source_link TEXT,
     votes INTEGER DEFAULT 0,
-    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_added TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Europe/Stockholm'),
     FOREIGN KEY (doc_id) REFERENCES docs(id) ON DELETE CASCADE
 );
 
-CREATE TABLE search_logs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS search_logs (
+    id SERIAL PRIMARY KEY,
     search_phrase VARCHAR(255) NOT NULL,
     results_count INTEGER NOT NULL,
-    search_time DATE DEFAULT (datetime('now','localtime'))
+    search_time TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Europe/Stockholm')
 );
